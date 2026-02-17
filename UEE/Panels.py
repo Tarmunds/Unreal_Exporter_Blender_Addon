5e3a68ea4a2e406adff397bec5844ed2ffc747ca
Kostia Perry <kostia@opusmajor.io>
2026-02-17 15:17:06 +0100

    changin export pannel

--- UEE/Panels.py
+++ UEE/Panels.py
@@ -38,11 +38,11 @@ class UEE_Panel(bpy.types.Panel):
         if box:
 
             row = go_to_row(box)
-            row.prop(uee_props, "y_up", text="Y-Up")
-            row.prop(uee_props, "join_meshes", text="Join Meshes at Export")
+            row.prop(uee_props, "y_up", text="Y-Up", toggle=True, icon='CHECKMARK' if uee_props.y_up else 'CANCEL')
+            row.prop(uee_props, "join_meshes", text="Join Meshes at Export", toggle=True, icon='CHECKMARK' if uee_props.join_meshes else 'CANCEL')
             row = go_to_row(box)
-            row.prop(uee_props, "include_transform", text="Include Location")
-            row.prop(uee_props, "include_curve", text="Include Curve Geometry")
+            row.prop(uee_props, "include_transform", text="Include Location", toggle=True, icon='CHECKMARK' if uee_props.include_transform else 'CANCEL')
+            row.prop(uee_props, "include_curve", text="Include Curve Geometry", toggle=True, icon='CHECKMARK' if uee_props.include_curve else 'CANCEL')
 
         row = go_to_row(layout)
         row.prop(uee_props, "export_collision", text=f"Collision Export {'Enable' if uee_props.export_collision else 'Disable'}", toggle=True, icon='MESH_CUBE' if uee_props.export_collision else 'CANCEL')
